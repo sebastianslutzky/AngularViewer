b06763b763fc18b973fee66f03261c6f1f5269a6
sebastianslutzky <sebastianslutzky@yahoo.com>
2017-11-14 00:00:30 +0000

    Adding table view for list action results

--- app/components/entity/entity.ts
+++ app/components/entity/entity.ts
@@ -11,8 +11,8 @@ import {Ng2TableModule} from 'ng2-table/ng2-table'
 export default class EntityComponent implements OnInit{ 
   _resource:XActionResultList
 
-  public columns : any[] 
-  public rows : any[] 
+  public columns : Array<any>
+  public rows : Array<any>
 
   constructor(public injector: Injector){
     let rawResult  = this.injector.get('actionResource').Result;
@@ -20,7 +20,7 @@ export default class EntityComponent implements OnInit{
     //todo: move to specialised class that decides how to cast result based on type
     this._resource = new XActionResultList(rawResult);
     this.rows  = this._resource.XListItems
-    this.columns = this._resource.PropertyNames.map(function(item){return{name: item}})
+    this.columns = this._resource.PropertyNames.map(function(item){return{name: item, title: item}})
   }
 
   ngOnInit(): void {
